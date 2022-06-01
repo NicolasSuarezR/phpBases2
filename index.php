a917f03fd6ffa838bffc19b23ca9ee0afc7969d4
Your Name <nasuarezro@unal.edu.co>
2022-06-01 02:45:16 -0500

    Iniciando Login

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<link rel="apple-touch-icon" sizes="76x76" href=".gitignore/assets/img/apple-icon.png">
-	<link rel="icon" type="image/png" href=".gitignore/assets/img/favicon.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" href="assets/img/favicon.png">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 	<title>Login Page - Material Kit PRO by Creative Tim</title>
@@ -13,8 +13,8 @@
 	<!--     Fonts and icons     -->
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
 	<!-- CSS Files -->
-    <link href=".gitignore/assets/css/bootstrap.min.css" rel="stylesheet" />
-    <link href=".gitignore/assets/css/material-kit.css?v=1.2.1" rel="stylesheet"/>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/material-kit.css?v=1.2.1" rel="stylesheet"/>
 </head>
 
 <body class="login-page">
@@ -107,12 +107,12 @@
     	</div>
     </nav>
 
-	<div class="page-header header-filter" style="background-image: url('.gitignore/assets/img/bg7.jpg'); background-size: cover; background-position: top center;">
+	<div class="page-header header-filter" style="background-image: url('assets/img/bg7.jpg'); background-size: cover; background-position: top center;">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
 					<div class="card card-signup">
-						<form class="form" method="" action="">
+						<form class="form">
 							<div class="header header-primary text-center">
 								<h4 class="card-title">Log in</h4>
 								<div class="social-line">
@@ -198,31 +198,31 @@
 
 </body>
 	<!--   Core JS Files   -->
-	<script src=".gitignore/assets/js/jquery.min.js" type="text/javascript"></script>
-	<script src=".gitignore/assets/js/bootstrap.min.js" type="text/javascript"></script>
-	<script src=".gitignore/assets/js/material.min.js"></script>
+	<script src="assets/js/jquery.min.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="assets/js/material.min.js"></script>
 
 	<!--    Plugin for Date Time Picker and Full Calendar Plugin   -->
-	<script src=".gitignore/assets/js/moment.min.js"></script>
+	<script src="assets/js/moment.min.js"></script>
 
 	<!--	Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/   -->
-	<script src=".gitignore/assets/js/nouislider.min.js" type="text/javascript"></script>
+	<script src="assets/js/nouislider.min.js" type="text/javascript"></script>
 
 	<!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker   -->
-	<script src=".gitignore/assets/js/bootstrap-datetimepicker.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap-datetimepicker.js" type="text/javascript"></script>
 
 	<!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select   -->
-	<script src=".gitignore/assets/js/bootstrap-selectpicker.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap-selectpicker.js" type="text/javascript"></script>
 
 	<!--	Plugin for Tags, full documentation here: http://xoxco.com/projects/code/tagsinput/   -->
-	<script src=".gitignore/assets/js/bootstrap-tagsinput.js"></script>
+	<script src="assets/js/bootstrap-tagsinput.js"></script>
 
 	<!--	Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput   -->
-	<script src=".gitignore/assets/js/jasny-bootstrap.min.js"></script>
+	<script src="assets/js/jasny-bootstrap.min.js"></script>
 
 	<!--    Plugin For Google Maps   -->
 	<script  type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
 
 	<!--    Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc    -->
-	<script src=".gitignore/assets/js/material-kit.js?v=1.2.1" type="text/javascript"></script>
+	<script src="assets/js/material-kit.js?v=1.2.1" type="text/javascript"></script>
 </html>
